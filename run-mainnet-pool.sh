8dfe527387de5c58217121aa5f63724755287b23
Caue Pereira Dos Santos <71650516+CauejK@users.noreply.github.com>
2021-09-13 07:57:08 -0300

    Update run-mainnet-pool.sh

--- run-mainnet-pool.sh
+++ run-mainnet-pool.sh
@@ -1,1 +1,1 @@
-./sugarmaker -a YespowerSugar -o stratum+tcp://1pool.sugarchain.org:3333 -u sugar1q033x33ghn6qkxhvumm6mkkza6nfr5qmgkkwlwp -t1
+./sugarmaker -a YespowerSugar -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q033x33ghn6qkxhvumm6mkkza6nfr5qmgkkwlwp -t8
